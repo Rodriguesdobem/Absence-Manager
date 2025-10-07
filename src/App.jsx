1f4050d275a153d44483322934bcdc34ac07b678
Rodriguesdobem <rm96930@estudante.fieb.edu.br>
2025-10-07 10:28:01 -0300

    Segundo Commit

--- src/App.jsx
+++ src/App.jsx
@@ -178,37 +178,71 @@ function CadastrarAluno() {
       </div>
       {menuOpen && <div className="overlay" onClick={() => setMenuOpen(false)}></div>}
       <div className="cadastro-content">
-        <div className="cadastro-form">
-          <h1>Cadastrar Novo Aluno</h1>
-          <p className="form-subtitle">Preencha os dados abaixo para adicionar um novo aluno ao sistema</p>
-          <div className="form-layout">
-            <div className="form-row">
-              <div className="input-group">
-                <input type="text" className="input-field" required />
-                <label className="input-label">Nome</label>
-              </div>
-              <div className="input-group">
-                <input type="text" className="input-field" required />
-                <label className="input-label">Sobrenome</label>
-              </div>
-            </div>
-            <div className="input-group full-width">
-              <input 
-                type="text" 
-                className="input-field" 
-                placeholder="" 
-                onFocus={(e) => e.target.placeholder = 'DD/MM/AAAA'}
-                onBlur={(e) => e.target.placeholder = ''}
-                required 
-              />
-              <label className="input-label">Data de Nascimento</label>
-            </div>
-            <div className="input-group full-width">
-              <input type="email" className="input-field" required />
-              <label className="input-label">E-mail</label>
-            </div>
+        <div className="container">
+          <div className="form-image">
+            <img src="assets/img/undraw_bookshelves_re_lxoy.svg" alt="" />
           </div>
-          <button className="cadastro-btn">Cadastrar Aluno</button>
+          <div className="form">
+            <form action="#">
+              <div className="form-header">
+                <div className="title">
+                  <h1>Cadastre-se</h1>
+                </div>
+              </div>
+              <div className="input-group">
+                <div className="input-box">
+                  <label htmlFor="firstname">Primeiro nome</label>
+                  <input id="firstname" type="text" name="firstname" placeholder="Digite seu primeiro nome" required />
+                </div>
+                <div className="input-box">
+                  <label htmlFor="lastname">Sobrenome</label>
+                  <input id="lastname" type="text" name="lastname" placeholder="Digite seu sobrenome" required />
+                </div>
+                <div className="input-box">
+                  <label htmlFor="email">E-mail</label>
+                  <input id="email" type="email" name="email" placeholder="Digite seu email" required />
+                </div>
+                <div className="input-box">
+                  <label htmlFor="number">Celular</label>
+                  <input id="number" type="tel" name="number" placeholder="(xx) xxxx-xxxx" required />
+                </div>
+                <div className="input-box">
+                  <label htmlFor="password">Senha</label>
+                  <input id="password" type="password" name="password" placeholder="Digite sua Senha" required />
+                </div>
+                <div className="input-box">
+                  <label htmlFor="confirmPassword">Confirme sua Senha</label>
+                  <input id="confirmPassword" type="password" name="confirmPassword" placeholder="Digite sua senha novamente" required />
+                </div>
+              </div>
+              <div className="gender-inputs">
+                <div className="gender-title">
+                  <h6>Gênero</h6>
+                </div>
+                <div className="gender-groups">
+                  <div className="gender-input">
+                    <input id="female" type="radio" name="gender" />
+                    <label htmlFor="female">Feminino</label>
+                  </div>
+                  <div className="gender-input">
+                    <input id="male" type="radio" name="gender" />
+                    <label htmlFor="male">Masculino</label>
+                  </div>
+                  <div className="gender-input">
+                    <input id="others" type="radio" name="gender" />
+                    <label htmlFor="others">Outros</label>
+                  </div>
+                  <div className="gender-input">
+                    <input id="none" type="radio" name="gender" />
+                    <label htmlFor="none">Prefiro não dizer</label>
+                  </div>
+                </div>
+              </div>
+              <div className="continue-button">
+                <button><a href="#">Continuar</a></button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </div>

b4984dc45052273fb28e74e6468a892dac9ba536
Rodriguesdobem <rm96930@estudante.fieb.edu.br>
2026-06-10 02:43:30 -0300

    Conexão Render

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,8 @@ export default defineConfig({
   server: {
     port: 5173,
     open: true
+  },
+  define: {
+    'import.meta.env.VITE_API_BASE_URL': JSON.stringify(process.env.VITE_API_BASE_URL || 'http://localhost:8080')
   }
 })
